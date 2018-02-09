724d1c18d87e61c21ac9cea409b964f76fd3ebb5
Estudiantes <Estudiantes@Labing50421.udistrital.edu.co>
2018-02-09 11:46:05 -0500

    tercercommit

---
+++ MiCalculadora/src/micalculadora/Principal.java
@@ -0,0 +1,11 @@
+package micalculadora;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // crear un objeto de la clase Calculadora
+        Calculadora micalculadora;  //definir el objeto
+        micalculadora = new Calculadora(); // construir el objeto
+
+    }
+}
