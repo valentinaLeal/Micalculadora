1ed339732e8b13a1ae6703f2115e1ebd344993a6
Estudiantes <Estudiantes@Labing70216>
2018-02-14 17:03:56 -0500

    Cuarto

---
+++ Calculadora 1/src/calculadora/pkg1/Calculadora.java
@@ -0,0 +1,30 @@
+
+package calculadora.pkg1;
+
+public class Calculadora {
+
+    int x;
+    int y;
+    int r;
+
+    void sumar() {
+
+        r = x + y;
+    }
+
+    void restar() {
+
+        if (x < y) {
+            int a;
+            a = x;
+            x = y;
+            y = a;
+        }
+        if (x < 0) {
+            x = -x;
+            y = -y;
+        }
+        r = x - y;
+    }
+
+}
---
+++ Calculadora 1/src/calculadora/pkg1/Principal.java
@@ -0,0 +1,11 @@
+package calculadora.pkg1;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // crear un objeto de la clase Calculadora
+        Calculadora micalculadora;  //definir el objeto
+        micalculadora = new Calculadora(); // construir el objeto
+
+    }
+}
